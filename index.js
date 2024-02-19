30eafb06ff4cdc29cd208ef2d2fe5fdca05b4dfb
hannahvw <103385894+hannahvw@users.noreply.github.com>
2024-02-19 15:39:42 -0600

    Added darkmode toggle

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const menuBtn = document.querySelector(".menu-icon");
 const closeBtn = document.querySelector(".close-button");
 const navMenu = document.querySelector(".nav-menu");
+const darkModeBtn = document.querySelector(".dark-mode-toggle");
+const lightModeBtn = document.querySelector(".light-mode-toggle");
 let toggleState = false;
 
 function toggleMenu() {
@@ -15,3 +17,19 @@ function toggleMenu() {
 
 menuBtn.addEventListener("click", toggleMenu);
 closeBtn.addEventListener("click", toggleMenu);
+
+function toggleDarkMode() {
+  toggleState = !toggleState;
+  if (toggleState == true) {
+    document.body.classList.add("darkmode");
+    darkModeBtn.style.display = "none";
+    lightModeBtn.style.display = "flex";
+  } else {
+    document.body.classList.remove("darkmode");
+    darkModeBtn.style.display = "flex";
+    lightModeBtn.style.display = "none";
+  }
+}
+
+darkModeBtn.addEventListener("click", toggleDarkMode);
+lightModeBtn.addEventListener("click", toggleDarkMode);
